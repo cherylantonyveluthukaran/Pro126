e707439b9fadfd1048d6b6d46708611462902ee6
cherylantonyveluthukaran <92528362+cherylantonyveluthukaran@users.noreply.github.com>
2022-10-27 12:48:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+music="";
+music_1="";
+
+function preload(){
+    music=loadSound("music.mp3");
+    music_1=loadSound("music2.mp3");
+}
+
 function setup(){
     canvas=createCanvas(600,600);
     canvas.center();
-    
+
     video=createCapture(VIDEO);
     video.hide();
-    
-    }
+}
+
+function draw(){
+    image(video,0,0,600,600);
+}
+
     
